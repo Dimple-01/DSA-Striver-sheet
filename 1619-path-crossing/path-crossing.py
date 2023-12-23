946528a146bae29c1cd3607f9fc03af08a5361b9
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2023-12-23 22:31:12 +0530

    Time: 42 ms (32.14%) | Memory: 17.6 MB (5.49%) - LeetSync

--- 1619-path-crossing/path-crossing.py
+++ 1619-path-crossing/path-crossing.py
@@ -1,22 +1,20 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        #Brute force
-        x,y=0,0
-        visited = {(0,0)}
-        for direction in path:
-            if direction == 'E':
-                x+=1
-            elif direction == 'W':
-                x-=1
-            elif direction == 'N':
+       # using set
+        res= set()
+        res.add((0,0))
+        x=0
+        y=0
+        for i in path:
+            if i == 'N':
                 y+=1
-            else:
+            elif i == 'S':
                 y-=1
-
-            if (x,y) in visited:
+            elif i == 'E':
+                x+=1
+            else:
+                x-=1
+            if (x,y) in res:
                 return True
-            
-            visited.add((x,y))
+            res.add((x,y))
         return False
-
-        
