23114f10e175debd87795c599f057719c5cb15d4
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2023-12-30 13:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (10.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
+    // DP solution ------------ top- down approach (memoization)
+int solver(int i, vector<int> & dp)
+        {
+            if(i==0 || i==1)
+            return i;
+            if(dp[i] !=-1) return dp[i];
+
+            dp[i]= solver(i-1, dp) + solver(i-2, dp);
+            return dp[i];
+        }
     int fib(int n) {
-        // brute force
+               vector<int> dp(n+1 ,-1);
+        return solver(n, dp);
+
+    }
+};
+        // brute force--------------
         // int f1=0,i,f2=1,fibo=0;
         // if(n==0)
         //     return 0;
@@ -15,10 +30,11 @@ public:
         // }
         // return fibo;
 
-        // recursive solution
-        if(n==0 || n==1)
-        return n;
-        return fib(n-1) + fib(n-2);
+        // recursive solution------------------
+        // if(n==0 || n==1)
+        // return n;
+        // return fib(n-1) + fib(n-2);
 
-    }
-};
+        
+        
+ 
