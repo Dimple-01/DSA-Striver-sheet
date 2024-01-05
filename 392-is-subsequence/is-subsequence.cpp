f5452188465dfdd134c8569e139d3c3b879e1ca8
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2024-01-05 15:37:57 +0530

    Time: 8 ms (5.42%) | Memory: 280.7 MB (5.34%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    bool solver(string &s, string &t, int i, int j, vector<bool>&dp)
+    bool solver(int i, int j,string s, string t )
     {
-        // dp solution
-        if(i==0) return true;
-        if(j==0 ) return false;
-        if(dp[j] !=-false) return true;
-
-        if(s[i-1] == t[j-1])
-            dp[j]= solver(s,t,i-1,j-1,dp);
+        if(i==s.size()) return true;
+        if(j==t.size())
+        {
+            return false;
+        }
+         if (s[i]== t[j])
+            return solver(i+1,j+1,s,t);
         else
-            dp[j]= solver(s,t,i,j-1,dp);
-        return dp[j];
-
-
+        return solver(i, j+1 ,s,t);
+        
     }
     bool isSubsequence(string s, string t) {
-    vector<bool> dp(t.size()+1, false);
-    return solver(s,t, s.size(), t.size(),dp);
-    
+        // recursion
+        return solver(0,0,s,t);
+             
     }
 };
