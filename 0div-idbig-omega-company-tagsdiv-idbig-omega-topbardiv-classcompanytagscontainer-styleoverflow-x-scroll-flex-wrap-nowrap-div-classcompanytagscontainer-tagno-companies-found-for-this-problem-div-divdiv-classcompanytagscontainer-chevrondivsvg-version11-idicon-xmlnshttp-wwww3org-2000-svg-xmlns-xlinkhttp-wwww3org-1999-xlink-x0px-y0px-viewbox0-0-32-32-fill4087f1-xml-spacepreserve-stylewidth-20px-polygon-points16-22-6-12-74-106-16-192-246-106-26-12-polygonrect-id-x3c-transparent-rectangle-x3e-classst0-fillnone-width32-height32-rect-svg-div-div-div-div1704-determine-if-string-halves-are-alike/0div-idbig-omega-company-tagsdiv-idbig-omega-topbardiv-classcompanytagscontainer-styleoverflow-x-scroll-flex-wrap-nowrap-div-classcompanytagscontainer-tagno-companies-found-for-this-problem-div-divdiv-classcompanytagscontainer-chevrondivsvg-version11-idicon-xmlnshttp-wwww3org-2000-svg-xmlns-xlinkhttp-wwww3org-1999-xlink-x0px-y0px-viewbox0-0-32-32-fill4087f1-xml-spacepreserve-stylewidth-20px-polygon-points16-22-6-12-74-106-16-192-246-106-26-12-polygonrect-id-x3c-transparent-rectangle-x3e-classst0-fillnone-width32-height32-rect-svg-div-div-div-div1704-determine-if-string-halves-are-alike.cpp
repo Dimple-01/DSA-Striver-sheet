9e276c79552e57fc34a871078e9735d0d6efbc4f
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2024-01-12 22:46:13 +0530

    Time: 6 ms (12.88%), Space: 7.2 MB (11.73%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        // Brute Force
-        int len=s.size();
-        int cnt=0,cnt1=0;
-        for(int i=0;i<len/2;i++)
+        // Brute Force ----O(n/2)=O(n)
+        
+        // int len=s.size();
+        // int cnt=0,cnt1=0;
+        // for(int i=0;i<len/2;i++)
+        // {
+        //     if(s[i]=='a' || s[i]== 'e' || s[i]=='i' || s[i]=='o'|| s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+        //     {
+        //         cnt++;
+        //     }
+        // }
+        // for(int i=len/2;i<len;i++)
+        // {
+        //     if(s[i]=='a' || s[i]== 'e' || s[i]=='i' || s[i]=='o'|| s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+        //     {
+        //         cnt1++;
+        //     }
+        // }
+        // return cnt==cnt1;
+        
+        // using set  -----O(n)
+        unordered_set<char> vowels={'a', 'e','i','o','u','A','E','I','O','U'};
+        int cnt=0;
+        for(int i=0;i<s.size();i++)
         {
-            if(s[i]=='a' || s[i]== 'e' || s[i]=='i' || s[i]=='o'|| s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            if(vowels.count(s[i])>0)
             {
-                cnt++;
+                cnt+=(i<s.size()/2)?1:-1;
             }
         }
-        for(int i=len/2;i<len;i++)
-        {
-            if(s[i]=='a' || s[i]== 'e' || s[i]=='i' || s[i]=='o'|| s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
-            {
-                cnt1++;
-            }
-        }
-        return cnt==cnt1;
+        
+        return cnt==0;
         
     }
 };
