363ef1b59955b0a6dce78e49e8e4c4b463dd46e7
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2024-02-05 21:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (15.16%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -12,34 +12,27 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        // swaping values of left and right within the same tree 
-        // O(n) time complexity
-        // O(n) space compexity
-        if(root )
+        queue<TreeNode*> q;
+
+        if(root == NULL)
         {
-            int s;
-            
-            stack<TreeNode*> st;
-            st.push(root);
-            while(! st.empty())
-            {
-                s= st.size();
-                TreeNode* newtree = st.top();
-                st.pop();
-
-                TreeNode* temp = newtree-> left;
-                newtree->left = newtree->right;
-                newtree->right = temp;
-
-                if(newtree->left != NULL) st.push(newtree->left);
-                if(newtree->right != NULL) st.push(newtree->right);
-
-                
-            }
-            
+           return root; 
         }
-         return root;
+        q.push(root);
+        while(!q.empty())
+        {
+           
+            TreeNode* temp= q.front();
+            q.pop();
+            
+            TreeNode* tempo=temp-> left;
+            temp-> left =temp->right;
+            temp->right= tempo;
+            if(temp->left != NULL) q.push(temp->left);
+            if(temp->right != NULL) q.push(temp->right);
 
+        }
+        return root;
         
     }
 };
