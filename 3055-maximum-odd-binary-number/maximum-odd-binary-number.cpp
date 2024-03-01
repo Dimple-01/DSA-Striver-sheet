f4eb320d45fc56805763bdda8b54080dbbc8e3e4
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2024-03-02 01:55:42 +0530

    Time: 4 ms (61.32%) | Memory: 8.9 MB (22.55%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-   
+    // O(n)
+    int digitcount(string s)
+    {
+        int len=s.size();
+        int count=0;
+        for(int i =0;i<len;i++)
+        {
+            if(s[i]=='1')
+                count++;
+        }
+        return count;
+    }
     string maximumOddBinaryNumber(string s) {
-       
-        int len= s.size();
-        sort(s.begin(),s.end());
-        s.pop_back();
-        reverse(s.begin(),s.end());
-        s+='1';
-        return s;
-
-        
+        string str="";
+         int len=s.size(),i,j;
+         int countone= digitcount(s);
+        int countzero= len - countone;
+        for(i=0;i<countone-1;i++)
+        {
+            str+='1';
+        }
+        for(j=i;j<len-1;j++)
+        {
+            str+='0';
+        }
+        str+='1';
+        return str;
     }
 };
