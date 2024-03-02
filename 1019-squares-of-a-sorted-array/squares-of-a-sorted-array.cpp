16bc94b1936bafa58dd848af98d8a272060f2703
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2024-03-02 14:29:40 +0530

    Time: 19 ms (83.11%) | Memory: 28.3 MB (58.32%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-       for(auto &n :nums)
-       {
-           n=abs(n) * abs(n);
-       } 
-       sort(nums.begin(),nums.end());
-       return nums;
+        //TC = O(n)
+        //sc= O(n)
+        int st=0,end=nums.size()-1;
+        int k= nums.size()-1;
+        vector<int> result(k+1);
+        while(st<=end)
+        {
+            int left_sq_ele= nums[st] * nums[st];
+            int right_sq_ele= nums[end] * nums[end];
+            if(left_sq_ele > right_sq_ele)
+            {
+                result[k--]= left_sq_ele;
+                st++;
+            }
+            else
+            {
+                result[k--] = right_sq_ele;
+                end--;
+            }
+        }
+
+        return result;
     }
 };
