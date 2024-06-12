b801455170ba4149be43a663348b14faaa08466e
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2024-06-12 13:17:46 +0530

    Time: 16 ms (54.24%) | Memory: 21.7 MB (38.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       
-        int n= nums.size();
-         vector<int> store;
-        for(int i=0;i<nums.size();i++)
+        int zerocount=count(nums.begin(),nums.end(),0);
+        nums.erase(remove(nums.begin(),nums.end(),0),nums.end());
+        for(int i=0;i<zerocount;i++)
         {
-            if(nums[i] != 0)
-            {
-                store.push_back(nums[i]);
-            }
+            nums.push_back(0);
         }
-        int store_len=store.size();
-        int diff= n - store_len;
-        for(int j=0;j<diff;j++)
-        {
-            store.push_back(0);
-        }
-        for(int i=0;i<n;i++)
-        {
-            nums[i]=store[i];
-        }
-        
     }
 };
